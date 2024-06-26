c44fd189ac1907b4ec260eb9dd5be1b389271bcd
Sivaraj.V <sivaraj.v@lseg.com>
2024-06-26 22:48:03 +0530

    Working

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 // server.js
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // Path to your db.json file
+const router = jsonServer.router('./db.json'); // Path to your db.json file
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
-module.exports = server;
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
+
+// Run node ./server.js
